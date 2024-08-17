1fed3bd73974686ebbaafc1a4b54ddcb18ec9a68
Olivier Poulin <456crdv@gmail.com>
2024-08-17 18:05:58 -0400

    SOUND

---
+++ Assets/Sound/Scripts/Music_bank.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music_bank : MonoBehaviour
+{
+    public static string MusicHouse = "event:/Music/House_background_music";
+    public static string MusicTree = "event:/Music/Tree_background_music";
+    public static string MusicTitle = "event:/Music/Title_Screen";
+}
---
+++ Assets/Sound/Scripts/SFX_bank.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFX_bank : MonoBehaviour
+{
+    public static string EventJump = "event:/SFX/Jump";
+    public static string EventWalk = "event:/SFX/Float_Move";
+    public static string EventWallClimb = "event:/SFX/Wall_climb";
+    public static string EventParachute = "event:/SFX/Parachute_Open";
+}
