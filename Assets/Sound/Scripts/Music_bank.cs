f02a7112d61c86458f5c9616eb9d49a781dc0bdd
Olivier Poulin <456crdv@gmail.com>
2024-08-17 19:49:50 -0400

    Music Manager Done + Scenes in Build Settings

--- Assets/Sound/Scripts/Music_bank.cs
+++ Assets/Sound/Scripts/Music_bank.cs
@@ -7,4 +7,8 @@ public class Music_bank : MonoBehaviour
     public static string MusicHouse = "event:/Music/House_background_music";
     public static string MusicTree = "event:/Music/Tree_background_music";
     public static string MusicTitle = "event:/Music/Title_Screen";
+    /*TO DO*/
+    public static string MusicTown = "";
+    public static string MusicForest = "";
+    public static string MusicWin = "";
 }
