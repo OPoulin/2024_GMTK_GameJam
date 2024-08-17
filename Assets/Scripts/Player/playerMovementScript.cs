f0574e682fb85c65ce3b1f0049eca9ef27ea6566
massimo <masst123543@gmail.com>
2024-08-17 12:20:35 -0400

    added gliderCloth

--- Assets/Scripts/Player/playerMovementScript.cs
+++ Assets/Scripts/Player/playerMovementScript.cs
@@ -22,53 +22,83 @@ public class playerMovementScript : MonoBehaviour
     public float wallMountDelay;
     public GameObject wallRideRight;
     public GameObject wallRideLeft;
-
     //affects mouvement and mounting
     bool mountControl = true;
 
+    //for glider cloth
+    bool isGliding = false;
+    public GameObject gliderSprite;
+    public float gliderFallSpeed;
+    public float gliderHorizontalSpeed;
+    float nonGlideSpeed;
+
     Rigidbody2D rb;
+
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        nonGlideSpeed = maxSpeed;
     }
 
 
     void Update()
     {
-        //wall jump
-        if(Physics2D.OverlapBox(new Vector2(transform.position.x, transform.position.y + 0.6f), new Vector2(1, 1), 0f) && !Physics2D.OverlapCircle(new Vector2(transform.position.x, transform.position.y - 0.499f), 0.15f) && isRunning)
-        {
-            if (mountControl)
-            {
-                //wall climb conditions complete
-                wallMounted = true;
-
-                rb.constraints = RigidbodyConstraints2D.FreezePositionY;
-                rb.gravityScale = 0;
-                rb.constraints = RigidbodyConstraints2D.FreezeRotation;
-                speed = 0;
-
-                GetComponent<SpriteRenderer>().enabled = false;
-                if(runDirection < 0) { wallRideLeft.SetActive(true); }
-                if (runDirection > 0) { wallRideRight.SetActive(true); }
-            }
-        }
-        else
-        {
-            if(rb.velocity.magnitude != 0)
-            {
-                //wallMounted determines if can jump when mounted on wall
-                wallMounted = false;
-                rb.gravityScale = 1;
-
-                GetComponent<SpriteRenderer>().enabled = true;
-                wallRideLeft.SetActive(false);
-                wallRideRight.SetActive(false);
-            }
-        }
+        //this works better than clamp for some reason
         if (speed > maxSpeed) { speed = maxSpeed; }
-        if(speed < -maxSpeed) { speed = -maxSpeed;}
+        if (speed < -maxSpeed) { speed = -maxSpeed; }
+
+
+
+        //wall jump
+        if (playerManagerScript.nailUnlcoked)
+        {
+            if (Physics2D.OverlapBox(new Vector2(transform.position.x, transform.position.y + 0.6f), new Vector2(1, 1), 0f) && !Physics2D.OverlapCircle(new Vector2(transform.position.x, transform.position.y - 0.499f), 0.15f) && isRunning)
+            {
+                if (mountControl)
+                {
+                    //wall climb conditions complete
+                    wallMounted = true;
+
+                    rb.constraints = RigidbodyConstraints2D.FreezePositionY;
+                    rb.gravityScale = 0;
+                    rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+                    speed = 0;
+
+                    GetComponent<SpriteRenderer>().enabled = false;
+                    if (runDirection < 0) { wallRideLeft.SetActive(true); }
+                    if (runDirection > 0) { wallRideRight.SetActive(true); }
+                }
+            }
+            else
+            {
+                if (rb.velocity.magnitude != 0)
+                {
+                    //wallMounted determines if can jump when mounted on wall
+                    wallMounted = false;
+                    rb.gravityScale = 1;
+
+                    GetComponent<SpriteRenderer>().enabled = true;
+                    wallRideLeft.SetActive(false);
+                    wallRideRight.SetActive(false);
+                }
+            }
+        }
+
+
+
+        //gliding
+        if (isGliding)
+        {
+            if(rb.velocity.y < 0)
+            {
+                startGliding();
+            }
+            if (Physics2D.OverlapCircle(new Vector2(transform.position.x, transform.position.y - 0.499f), 0.15f) || wallMounted)
+            {
+                stopGliding();
+            }
+        }
     }
 
 
@@ -148,6 +178,41 @@ public class playerMovementScript : MonoBehaviour
         }
     }
 
+
+
+    //gliding
+    public void onGlide(InputAction.CallbackContext context)
+    {
+        if (playerManagerScript.clothUnlocked)
+        {
+            if (context.performed)
+            {
+                //if not on floor and not wall mounted
+                if (!Physics2D.OverlapCircle(new Vector2(transform.position.x, transform.position.y - 0.499f), 0.15f) && !wallMounted)
+                {
+                    isGliding = true;
+                }
+            }
+            if (context.canceled)
+            {
+                stopGliding();
+            }
+        }
+    }
+    void startGliding()
+    {
+        gliderSprite.SetActive(true);
+        rb.drag = gliderFallSpeed*-1;
+        maxSpeed = gliderHorizontalSpeed;
+    }
+    void stopGliding()
+    {
+        isGliding = false;
+
+        gliderSprite.SetActive(false);
+        rb.drag = 0;
+        maxSpeed = nonGlideSpeed;
+    }
 
 
     //funnction called by wall jumping
