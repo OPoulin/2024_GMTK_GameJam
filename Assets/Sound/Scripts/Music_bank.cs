c720e182a8dc6665c0ec3a01735696cea506c5ec
Olivier Poulin <456crdv@gmail.com>
2024-08-17 21:12:03 -0400

    Music complete

--- Assets/Sound/Scripts/Music_bank.cs
+++ Assets/Sound/Scripts/Music_bank.cs
@@ -8,7 +8,7 @@ public class Music_bank : MonoBehaviour
     public static string MusicTree = "event:/Music/Tree_background_music";
     public static string MusicTitle = "event:/Music/Title_Screen";
     /*TO DO*/
-    public static string MusicTown = "";
-    public static string MusicForest = "";
-    public static string MusicWin = "";
+    public static string MusicTown = "event:/Music/Town_background_music";
+    public static string MusicForest = "event:/Music/Forest_background_music";
+    public static string MusicWin = "event:/Music/Win_background_music";
 }
