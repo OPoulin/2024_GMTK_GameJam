4477790e65c36e1c6d6b0525ea3096d15c2330f4
Olivier Poulin <456crdv@gmail.com>
2024-08-17 23:49:33 -0400

    Title screen design complete

--- Assets/Scripts/Sky/MovingSkyboxHouse.cs
+++ Assets/Scripts/Sky/MovingSkyboxHouse.cs
@@ -22,51 +22,74 @@ public class MovingSkyboxHouse : MonoBehaviour
     float positionClouds2;
     float positionClouds3;
 
+    public static UnityEngine.SceneManagement.Scene sceneTitle;
+
     // Start is called before the first frame update
     void Start()
     {
-        positionSky2 = skyBackgrounds[1].transform.position.x;
-        positionSky3 = skyBackgrounds[2].transform.position.x;
-        
-        positionStar2 = skyStars[1].transform.position.x;
-        positionStar3 = skyStars[2].transform.position.x;
-        
-        positionClouds2 = skyClouds[1].transform.position.x;
-        positionClouds3 = skyClouds[2].transform.position.x;
+        print(sceneTitle.buildIndex);
+        if (sceneTitle.buildIndex != -1)
+        {
+            positionSky2 = skyBackgrounds[1].transform.position.x;
+            positionSky3 = skyBackgrounds[2].transform.position.x;
+
+            positionStar2 = skyStars[1].transform.position.x;
+            positionStar3 = skyStars[2].transform.position.x;
+        }
+            positionClouds2 = skyClouds[1].transform.position.x;
+            positionClouds3 = skyClouds[2].transform.position.x;
+        print(positionClouds2);
+        print(positionClouds3);
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        /*Reset Background*/
-        if (skyBackgrounds[0].transform.position.x <= positionSky2)
-        {
-            skyBackgrounds[0].transform.position = new Vector2(positionSky3, skyBackgrounds[0].transform.position.y);
-        }
+        if (sceneTitle.buildIndex != -1) 
+        { 
+            /*Reset Background*/
+            if (skyBackgrounds[0].transform.position.x <= positionSky2)
+            {
+                skyBackgrounds[0].transform.position = new Vector2(positionSky3, skyBackgrounds[0].transform.position.y);
+            }
 
-        if (skyBackgrounds[1].transform.position.x <= positionSky2)
-        {
-            skyBackgrounds[1].transform.position = new Vector2(positionSky3, skyBackgrounds[0].transform.position.y);
-        }
-        /*Reset Stars*/
-        if (skyStars[0].transform.position.x <= positionStar2)
-        {
-            skyStars[0].transform.position = new Vector2(positionStar3, skyStars[0].transform.position.y);
-        }
+            if (skyBackgrounds[1].transform.position.x <= positionSky2)
+            {
+                skyBackgrounds[1].transform.position = new Vector2(positionSky3, skyBackgrounds[0].transform.position.y);
+            }
+            /*Reset Stars*/
+            if (skyStars[0].transform.position.x <= positionStar2)
+            {
+                skyStars[0].transform.position = new Vector2(positionStar3, skyStars[0].transform.position.y);
+            }
 
-        if (skyStars[1].transform.position.x <= positionStar2)
-        {
-            skyStars[1].transform.position = new Vector2(positionStar3, skyStars[0].transform.position.y);
-        }
-        /*Reset Clouds*/
-        if (skyClouds[0].transform.position.x <= positionClouds1)
-        {
-            skyClouds[0].transform.position = new Vector2(positionClouds3, skyClouds[0].transform.position.y);
-        }
+            if (skyStars[1].transform.position.x <= positionStar2)
+            {
+                skyStars[1].transform.position = new Vector2(positionStar3, skyStars[0].transform.position.y);
+            }
+            /*Reset Clouds*/
+            if (skyClouds[0].transform.position.x <= positionClouds1)
+            {
+                skyClouds[0].transform.position = new Vector2(positionClouds3, skyClouds[0].transform.position.y);
+            }
 
-        if (skyClouds[1].transform.position.x <= positionClouds1)
+            if (skyClouds[1].transform.position.x <= positionClouds1)
+            {
+                skyClouds[1].transform.position = new Vector2(positionClouds3, skyClouds[0].transform.position.y);
+            }
+        }
+        else
         {
-            skyClouds[1].transform.position = new Vector2(positionClouds3, skyClouds[0].transform.position.y);
+            /*Reset Clouds*/
+            if (skyClouds[0].transform.position.x <= positionClouds2)
+            {
+                skyClouds[0].transform.position = new Vector2(positionClouds3, skyClouds[0].transform.position.y);
+            }
+
+            if (skyClouds[1].transform.position.x <= positionClouds2)
+            {
+                skyClouds[1].transform.position = new Vector2(positionClouds3, skyClouds[0].transform.position.y);
+            }
         }
 
         /*Moving the elements*/
@@ -84,13 +107,16 @@ public class MovingSkyboxHouse : MonoBehaviour
         foreach (var z in skyClouds)
         {
             z.transform.position = new Vector2(z.transform.position.x - moveSpeed, z.transform.position.y);
+            print(z.transform.position.x);
         }
 
         /*Keep them in place*/
-
+        if(sceneTitle.buildIndex != -1)
+        {
         skyBackgrounds[2].transform.position = new Vector2(positionSky3, skyBackgrounds[0].transform.position.y);
-        skyClouds[2].transform.position = new Vector2(positionClouds3, skyClouds[0].transform.position.y);
         skyStars[2].transform.position = new Vector2(positionStar3, skyStars[0].transform.position.y);
+        }
+        skyClouds[2].transform.position = new Vector2(positionClouds3, skyClouds[0].transform.position.y);
 
     }
 }
