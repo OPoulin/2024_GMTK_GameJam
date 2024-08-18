16ec90cbf2351cb74a968e1026259883fee4ff07
massimo <masst123543@gmail.com>
2024-08-18 16:49:58 -0400

    invoked death

--- Assets/Scripts/Player/playerDamageScript.cs
+++ Assets/Scripts/Player/playerDamageScript.cs
@@ -2,12 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class playerDamageScript : MonoBehaviour
 {
     public int damage;
     public float iFrameTime;
     public bool iFramesActive = false;
+
+    public GameObject deathSmokeEffect;
+    public GameObject nail1;
+    public GameObject nail2;
+    public GameObject glider;
+    public GameObject feather;
 
     void OnCollisionEnter2D(Collision2D collision)
     {
@@ -54,5 +61,21 @@ public class playerDamageScript : MonoBehaviour
         GetComponent<Animator>().SetTrigger("Death");
         GetComponent<playerMovementScript>().isDead = true;
         GetComponent<Rigidbody2D>().drag = 10;
+
+        Invoke("pouf", 1);
+    }
+    void pouf()
+    {
+        deathSmokeEffect.SetActive(true);
+        GetComponent<SpriteRenderer>().enabled = false;
+        nail1.SetActive(false);
+        nail2.SetActive(false);
+        glider.SetActive(false);
+        feather.SetActive(false);
+        Invoke("resetScene", 1.25f);
+    }
+    void resetScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
